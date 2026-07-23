51c82dd85b926b8dbc16d228d92a3156a266220d
viplavg <viplavg1999@gmail.com>
2026-07-23 21:56:18 +0530

    Some Manager role specific feature API implemented

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -81,3 +81,24 @@ export const loginUser = async (req, res) => {
         });
     }
 }
+
+export const getEmployees = async (req, res) => {
+  try {
+    const employees = await User.find({
+      role: "EMPLOYEE",
+    })
+      .select("_id name email")
+      .sort({ name: 1 });
+
+    return res.status(200).json({
+      success: true,
+      count: employees.length,
+      data: employees,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
